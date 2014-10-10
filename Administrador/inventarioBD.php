ead2a1aaebc37e4727e1b53ddf0cc161d1c24c4e
Edixon <edixon.hernandez.c@gmail.com>
2014-10-09 20:45:02 -0500

    inventario coreccion

--- Administrador/inventarioBD.php
+++ Administrador/inventarioBD.php
@@ -4,13 +4,18 @@
   $inventario = mysql_query("SELECT * FROM inventario",$cn);
   
   $productos = array();
-  $aux=0;
-
+  
   while ($row = mysql_fetch_assoc($inventario)) {
   	$aux1=$row['codigo'];
-  	if($aux != $aux1){
-  		array_push($productos, $row['codigo']);
-  	}
-  	$aux=$aux1;
+  	$i=0;
+    foreach ($productos as $key => $valor) {
+      if($aux1 == $valor){
+        $i=1;
+      }
+    }
+    if($i==0){
+      array_push($productos, $row['codigo']);
+      echo $row['codigo'];
+    }
   }
  ?>
--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$cn = mysql_connect("localhost","root","programacion2")or die("Error en Conexion");
+$cn = mysql_connect("localhost","root","12345")or die("Error en Conexion");
 $db = mysql_select_db("smartsolutions")or die("Error en Db".mysql_error());
 return($cn);
 return($db);
